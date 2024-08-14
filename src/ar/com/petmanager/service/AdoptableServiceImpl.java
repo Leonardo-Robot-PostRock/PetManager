e038c5155dc7b60f5f01dc38c3f27519e4dbee13
Leonardo <leonardo38361@gmail.com>
2024-08-13 23:49:59 -0300

    creación de implementación de servicio

--- src/ar/com/petmanager/service/AdoptableServiceImpl.java
+++ src/ar/com/petmanager/service/AdoptableServiceImpl.java
@@ -1,5 +1,23 @@
 package ar.com.petmanager.service;
 
-public interface AdoptableService {
+import java.util.ArrayList;
+import java.util.List;
+import ar.com.petmanager.domain.*;
 
+public class AdoptableServiceImpl implements AdoptableService {
+	private List<Pet> availablePets;
+
+	public AdoptableServiceImpl() {
+		this.availablePets = new ArrayList<>();
+	}
+
+	@Override
+	public void adopPet(Pet pet) {
+		availablePets.remove(pet);
+	}
+
+	@Override
+	public List<Pet> listAvailablePets() {
+		return new ArrayList<>(availablePets);
+	}
 }
