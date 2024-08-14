0a1d67e6c7ca04a9f471aa4c721726814dfe592a
Leonardo <leonardo38361@gmail.com>
2024-08-14 16:38:46 -0300

    relación de composición

--- src/ar/com/petmanager/domain/Owner.java
+++ src/ar/com/petmanager/domain/Owner.java
@@ -1,5 +1,38 @@
 package ar.com.petmanager.domain;
 
 public class Owner {
-	
+	private String dni;
+	private String name;
+	private Address address;
+
+	public Owner(String dni, String name, String street, String city) {
+		this.dni = dni;
+		this.name = name;
+		Address address = new Address(street, city);
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
 }
