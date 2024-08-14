d0aa4f854161ac87770280275e2d6fc2f9fd6c62
Leonardo <leonardo38361@gmail.com>
2024-08-13 23:49:15 -0300

    creación de clase gato

--- src/ar/com/petmanager/domain/Cat.java
+++ src/ar/com/petmanager/domain/Cat.java
@@ -1,10 +1,8 @@
 package ar.com.petmanager.domain;
 
 public class Cat extends Pet {
-	private String race;
-
 	public Cat(String name, int age, double weight, String health, String race) {
-		super(name, age, weight, health,race);
+		super(name, age, weight, health, race);
 	}
 
 }
