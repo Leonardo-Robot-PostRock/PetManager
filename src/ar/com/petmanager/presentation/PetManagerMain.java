39fcc2e1f34d832187801cb5207c613f92129389
Leonardo <leonardo38361@gmail.com>
2024-08-16 22:46:14 -0300

    main

---
+++ src/ar/com/petmanager/presentation/PetManagerMain.java
@@ -0,0 +1,10 @@
+package ar.com.petmanager.presentation;
+
+import ar.com.petmanager.gui.PetManagerUI;
+
+public class PetManagerMain {
+	public static void main(String[] args) {
+		PetManagerUI ui = new PetManagerUI();
+		ui.setVisible(true);
+	}
+}
