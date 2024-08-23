690b0e8bd1328f9f51feddb9498ffbb405b7d1a8
Leonardo <leonardo38361@gmail.com>
2024-08-23 18:58:59 -0300

    implementar métodos

--- src/ar/com/petmanager/domain/Donor.java
+++ src/ar/com/petmanager/domain/Donor.java
@@ -6,4 +6,53 @@ public class Donor extends Person {
 		super(dni, name, surname, phone, street, city);
 	}
 
+	@Override
+	public int getDni() {
+		return super.getDni();
+	}
+
+	@Override
+	public void setDni(int dni) {
+		super.setDni(dni);
+	}
+
+	@Override
+	public String getName() {
+		return super.getName();
+	}
+
+	@Override
+	public void setName(String name) {
+		super.setName(name);
+	}
+
+	@Override
+	public String getSurname() {
+		return super.getSurname();
+	}
+
+	@Override
+	public void setSurname(String surname) {
+		super.setSurname(surname);
+	}
+
+	@Override
+	public int getPhone() {
+		return super.getPhone();
+	}
+
+	@Override
+	public void setPhone(int phone) {
+		super.setPhone(phone);
+	}
+
+	@Override
+	public Address getAddress() {
+		return super.getAddress();
+	}
+
+	@Override
+	public void setAddress(Address address) {
+		super.setAddress(address);
+	}
 }
