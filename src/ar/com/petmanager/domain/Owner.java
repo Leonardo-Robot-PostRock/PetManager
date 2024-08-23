cebe389869ad5f8e37c5e9ad7af2af78e4984438
Leonardo <leonardo38361@gmail.com>
2024-08-23 19:01:46 -0300

    modificación de constructor y se implementan métodos

--- src/ar/com/petmanager/domain/Owner.java
+++ src/ar/com/petmanager/domain/Owner.java
@@ -7,9 +7,8 @@ public class Owner extends Person {
     private Vet preferredVet;
     private List<Pet> pets;
 
-    public Owner(int dni, String name, String surname, int phone, String city, String address) {
-        super(dni, name, surname, phone, city, address);
-        this.pets = new ArrayList<>();
+    public Owner(int dni, String name, String surname, int phone, String street, String city) {
+        super(dni, name, surname, phone, street, city);
     }
 
     public Vet getPreferredVet() {
@@ -31,4 +30,53 @@ public class Owner extends Person {
         return pets;
     }
 
+    @Override
+    public int getDni() {
+        return super.getDni();
+    }
+
+    @Override
+    public void setDni(int dni) {
+        super.setDni(dni);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public String getSurname() {
+        return super.getSurname();
+    }
+
+    @Override
+    public void setSurname(String surname) {
+        super.setSurname(surname);
+    }
+
+    @Override
+    public int getPhone() {
+        return super.getPhone();
+    }
+
+    @Override
+    public void setPhone(int phone) {
+        super.setPhone(phone);
+    }
+
+    @Override
+    public Address getAddress() {
+        return super.getAddress();
+    }
+
+    @Override
+    public void setAddress(Address address) {
+        super.setAddress(address);
+    }
 }
