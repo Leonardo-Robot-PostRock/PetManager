de1f9d0e4ee4bc2c837ecd59d4712212cceda033
Leonardo <leonardo38361@gmail.com>
2024-08-26 23:24:45 -0300

    implementación de pet service

--- src/ar/com/petmanager/service/PetServiceImpl.java
+++ src/ar/com/petmanager/service/PetServiceImpl.java
@@ -2,6 +2,7 @@ package ar.com.petmanager.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import ar.com.petmanager.domain.*;
 
@@ -9,15 +10,15 @@ public class PetServiceImpl implements PetService {
     private List<Pet> availablePets;
 
     public PetServiceImpl() {
-        this.availablePets = new ArrayList<>();
+        this.availablePets = new ArrayList<Pet>();
     }
 
     @Override
     public void addOwner(Owner owner, Pet pet) {
-        
+
         if (owner == null || owner.getDni() <= 0) return;
 
-        if(!availablePets.contains(pet)) return;
+        if (!availablePets.contains(pet)) return;
 
         owner.adoptPet(pet);
         availablePets.remove(pet);
@@ -29,27 +30,38 @@ public class PetServiceImpl implements PetService {
     }
 
     @Override
-    public void add(Pet entity) {
-
+    public void add(Pet pet) {
+        availablePets.add(pet);
     }
 
     @Override
-    public void delete(Pet entity) {
-
+    public void delete(Pet pet) {
+        availablePets.remove(pet);
     }
 
     @Override
     public Pet getById(int id) {
-        return null;
+        return availablePets.stream().filter(pet -> pet.getId() == id).findFirst().orElse(null);
     }
 
     @Override
     public List<Pet> getAll() {
-        return null;
+        return new ArrayList<>(availablePets);
     }
 
     @Override
-    public void update(Pet entity) {
+    public void update(Pet pet) {
+        Optional<Pet> optionalPet = availablePets.stream().filter(existingPet -> pet.getId() == existingPet.getId()).findFirst();
 
+        if (optionalPet.isPresent()) {
+            Pet foundPet = optionalPet.get();
+
+            foundPet.setName(pet.getName());
+            foundPet.setAge(pet.getAge());
+            foundPet.setRace(pet.getRace());
+            foundPet.setWeight(pet.getWeight());
+            foundPet.setSick(pet.isSick());
+            foundPet.setDescription(pet.getDescription());
+        }
     }
 }
