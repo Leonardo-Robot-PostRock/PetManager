ae7f314af98fb075747e8fb71e5a62ba709a2b29
Leonardo <leonardo38361@gmail.com>
2024-08-26 23:23:42 -0300

    se agrega método toString

--- src/ar/com/petmanager/domain/Address.java
+++ src/ar/com/petmanager/domain/Address.java
@@ -24,5 +24,13 @@ public class Address {
 	public void setCity(String city) {
 		this.city = city;
 	}
-	
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Address{");
+		sb.append("street='").append(street).append('\'');
+		sb.append(", city='").append(city).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
 }
