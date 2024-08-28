2b5d54c095d6ab7525ba863d49ac209230b58c06
Leonardo <leonardo38361@gmail.com>
2024-08-28 04:48:02 -0300

    separación de lógica

--- src/ar/com/petmanager/gui/ownerUI/OwnerFormPanel.java
+++ src/ar/com/petmanager/gui/ownerUI/OwnerFormPanel.java
@@ -6,102 +6,30 @@ import ar.com.petmanager.service.OwnerServiceImpl;
 import ar.com.petmanager.service.VetServiceImpl;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class OwnerFormPanel extends JPanel {
-
-    private JTextField txtDni;
-    private JTextField txtName;
-    private JTextField txtSurname;
-    private JTextField txtPhone;
-    private JTextField txtStreet;
-    private JTextField txtCity;
-    private JComboBox<Vet> selectPreferredVet;
-
-    private OwnerServiceImpl ownerService;
-    private VetServiceImpl vetService;
+public class OwnerFormPanel extends OwnerPanelBase {
+    private final OwnerServiceImpl ownerService;
 
     public OwnerFormPanel(OwnerServiceImpl ownerService, VetServiceImpl vetService) {
+        super(vetService);
         this.ownerService = ownerService;
-        this.vetService = vetService;
 
-        setLayout(new GridBagLayout());
-        GridBagConstraints gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.insets = new Insets(5, 5, 5, 5);
-
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        add(new JLabel("DNI:"), gridBagConstraints);
-        txtDni = new JTextField(15);
-        gridBagConstraints.gridx = 1;
-        add(txtDni, gridBagConstraints);
-
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        add(new JLabel("Nombre:"), gridBagConstraints);
-        txtName = new JTextField(15);
-        gridBagConstraints.gridx = 1;
-        add(txtName, gridBagConstraints);
-
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        add(new JLabel("Apellido:"), gridBagConstraints);
-        txtSurname = new JTextField(15);
-        gridBagConstraints.gridx = 1;
-        add(txtSurname, gridBagConstraints);
-
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 3;
-        add(new JLabel("Teléfono:"), gridBagConstraints);
-        txtPhone = new JTextField(15);
-        gridBagConstraints.gridx = 1;
-        add(txtPhone, gridBagConstraints);
-
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 4;
-        add(new JLabel("Calle:"), gridBagConstraints);
-        txtStreet = new JTextField(15);
-        gridBagConstraints.gridx = 1;
-        add(txtStreet, gridBagConstraints);
-
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 5;
-        add(new JLabel("Ciudad:"), gridBagConstraints);
-        txtCity = new JTextField(15);
-        gridBagConstraints.gridx = 1;
-        add(txtCity, gridBagConstraints);
-
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 6;
-        add(new JLabel("Veterinario Preferido:"), gridBagConstraints);
-        selectPreferredVet = new JComboBox<>();
-        selectPreferredVet.setPreferredSize(new Dimension(170,20));
-        populateVets();
-        gridBagConstraints.gridx = 1;
-        add(selectPreferredVet, gridBagConstraints);
+        setPanelTitle("Agregar dueño");
 
         JButton btnSaveOwner = new JButton("Guardar");
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 7;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.anchor = GridBagConstraints.CENTER;
-        add(btnSaveOwner, gridBagConstraints);
-        btnSaveOwner.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                addOwner();
-            }
-        });
-    }
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 10;
+        gbc.gridwidth = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        add(btnSaveOwner, gbc);
 
-    private void populateVets() {
-        for (Vet vet : vetService.getAll()) {
-            selectPreferredVet.addItem(vet);
-        }
+        btnSaveOwner.addActionListener(e -> addOwner());
+        loadVets();
     }
 
     private void addOwner() {
