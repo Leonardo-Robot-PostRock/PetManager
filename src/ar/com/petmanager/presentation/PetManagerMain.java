c8c26c3e3f3549d11aef2a2f75578834d7e8da95
Leonardo <leonardo38361@gmail.com>
2024-08-30 01:13:36 -0300

    optimización de código

--- src/ar/com/petmanager/presentation/PetManagerMain.java
+++ src/ar/com/petmanager/presentation/PetManagerMain.java
@@ -2,9 +2,10 @@ package ar.com.petmanager.presentation;
 
 import ar.com.petmanager.gui.PetManagerUI;
 
+import javax.swing.*;
+
 public class PetManagerMain {
 	public static void main(String[] args) {
-		PetManagerUI ui = new PetManagerUI();
-		ui.setVisible(true);
+		SwingUtilities.invokeLater(PetManagerUI::new);
 	}
 }
